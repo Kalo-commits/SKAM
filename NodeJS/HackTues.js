4ce5f06bff07940366853667a2726935c8a3df31
MonikaSpasova <61327146+MonikaSpasova@users.noreply.github.com>
2021-03-14 00:41:15 +0200

    Update HackTues.js

--- NodeJS/HackTues.js
+++ NodeJS/HackTues.js
@@ -3,8 +3,6 @@ const app = express();
 const port = 8080;
 const path = require('path');
 const bodyParser = require('body-parser');
-const Router = require('router');
-const router = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, '0.0.0.0', function() { 
@@ -24,9 +22,6 @@ app.get('/main.html', (req, res) => {
     res.sendFile('./main.html', { root: __dirname });
 });
 
-app.get('/page2.html', (req, res) => {
-  res.sendFile('./page2.html', { root: __dirname });
-});
 
 app.get('/about.html', (req, res) => {
   res.sendFile('./about.html', { root: __dirname });
@@ -35,40 +30,40 @@ app.get('/about.html', (req, res) => {
 app.get('/password_match.js', (req, res) => {
   res.sendFile('./password_match.js', { root: __dirname });
 });
-app.use('/css',express.static(__dirname +'/style.css'));
+app.use('/style.css',express.static(__dirname +'/style.css'));
 
 
 let mysql = require('mysql');
 
-let con = mysql.createConnection({
+let connection = mysql.createConnection({
   host: '192.168.23.108',
   user: 'kaloyan',
   password: 'skam123',
   database: 'database1'
 })
+
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname +'/register.html'));
-});
+    res.sendFile(path.join(__dirname+'/signup.html'));
+  });
 app.post('/login.html',function(req,res){
-    var reg={
-        ID: 10,
-        EmailAddress : req.body.email,
-        PasswordSalt : req.body.password
-    };
 
     var email=req.body.email;
     var password=req.body.password;
-    con.query('SELECT * FROM users WHERE EmailAddress = ? and PasswordSalt= ?',  [email,password]
-,function(err,rows){
+    res.sendFile('/login.html',{root:__dirname});
+    var i =Number(5666);  
+    connection.connect(function(err) {
+    if (err) throw err;
+    connection.query('SELECT * FROM users WHERE EmailAddress = ? and PasswordSalt= ?',  [email,password]
+    ,function(err,rows){
     if(err) {
-        con.end();
+        connection.end();
         return console.log(err);
     }
 
     if (!rows.length)
     {
-        var sql = "INSERT INTO users (ID,EmailAddress, PasswordSalt,PasswordHash) VALUES ('100', '"+email+"','"+password+"','')";
-    con.query(sql, function (err, result) {
+        var sql = "INSERT INTO users (ID,EmailAddress, PasswordSalt,PasswordHash) VALUES ('1004', '"+email+"','"+password+"','')";
+    connection.query(sql, function (err, result) {
       if (err) throw err;
       console.log("1 record inserted");
        res.end();
@@ -76,9 +71,9 @@ app.post('/login.html',function(req,res){
     }
     else
     {
-        con.end();
+        connection.end();
         return res.sendFile("./login.html",{root:__dirname});
     }
-});
-
-});
+    });
+    });
+  });
