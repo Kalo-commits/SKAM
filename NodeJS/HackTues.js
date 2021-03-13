c36ef7ce79447bfd8a2fce2aca8639dd65c34430
Kalo-commits <61322511+Kalo-commits@users.noreply.github.com>
2021-03-13 18:28:53 +0200

    Added email database check

--- NodeJS/HackTues.js
+++ NodeJS/HackTues.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 8080;
 const path = require('path');
 const bodyParser = require('body-parser');
+const Router = require('router');
+const router = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, '0.0.0.0', function() { 
@@ -22,6 +24,9 @@ app.get('/main.html', (req, res) => {
     res.sendFile('./main.html', { root: __dirname });
 });
 
+app.get('/page2.html', (req, res) => {
+  res.sendFile('./page2.html', { root: __dirname });
+});
 
 app.get('/about.html', (req, res) => {
   res.sendFile('./about.html', { root: __dirname });
@@ -35,49 +40,45 @@ app.use('/css',express.static(__dirname +'/style.css'));
 
 let mysql = require('mysql');
 
-let connection = mysql.createConnection({
+let con = mysql.createConnection({
   host: '192.168.23.108',
   user: 'kaloyan',
   password: 'skam123',
-  database: 'HackTues'
+  database: 'database1'
 })
-
 app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/signup.html'));
-  });
+    res.sendFile(path.join(__dirname +'/register.html'));
+});
 app.post('/login.html',function(req,res){
+    var reg={
+        ID: 10,
+        EmailAddress : req.body.email,
+        PasswordSalt : req.body.password
+    };
 
     var email=req.body.email;
     var password=req.body.password;
-    res.sendFile('/login.html',{root:__dirname});
-    var i =Number(20);  
-    connection.connect(function(err) {
-    if (err) throw err;
-    var sql = "INSERT INTO users (ID,EmailAddress, PasswordSalt,PasswordHash) VALUES ('"+i+"', '"+email+"','"+password+"','')";
-    connection.query(sql, function (err, result) {
+    con.query('SELECT * FROM users WHERE EmailAddress = ? and PasswordSalt= ?',  [email,password]
+,function(err,rows){
+    if(err) {
+        con.end();
+        return console.log(err);
+    }
+
+    if (!rows.length)
+    {
+        var sql = "INSERT INTO users (ID,EmailAddress, PasswordSalt,PasswordHash) VALUES ('100', '"+email+"','"+password+"','')";
+    con.query(sql, function (err, result) {
       if (err) throw err;
       console.log("1 record inserted");
        res.end();
-       i++;
-    });
-    });
-  })
- /* app.get('/',function(req,res){
-    res.sendFile(path.join(__dirname+'/signup.html'));
-  
-  connection.query('select * from users where EmailAddress= "'+req.body.email+'" ',function(err,row){
-    if(err){
-      connnection.query('insert into users(ID,EmailAddress, PasswordSalt,PasswordHash) VALUES("","' + password + '","' + email + '","")',
-      function(err,rows,fields){   
-        console.log(rows);
-        res.send("inserted");
-    });
-  }
-  else{
-    res.send("user already registered");
-    return;
-  }
-  });
+        });
+    }
+    else
+    {
+        con.end();
+        return res.sendFile("./login.html",{root:__dirname});
+    }
+});
 
-  connection.end();
-});*/
+});
