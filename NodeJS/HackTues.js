917872981c8822ce4aa460bca1b545df7b3724b8
Kalo-commits <61322511+Kalo-commits@users.noreply.github.com>
2021-03-13 12:58:38 +0200

    UserName/Password Chech System

--- NodeJS/HackTues.js
+++ NodeJS/HackTues.js
@@ -1,7 +1,9 @@
-
 const express = require('express');
 const app = express();
 const port = 8080;
+const path = require('path');
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.listen(port, '0.0.0.0', function() { 
 
@@ -31,27 +33,54 @@ app.get('/about.html', (req, res) => {
 app.get('/password_match.js', (req, res) => {
   res.sendFile('./password_match.js', { root: __dirname });
 });
-//app.listen(port, () => console.log(`listening on port ${port}!`))
+app.use('/css',express.static(__dirname +'/style.css'));
+
 
 let mysql = require('mysql');
 
 let connection = mysql.createConnection({
-  host: '192.168.23.104',
+  host: '192.168.23.108',
   user: 'kaloyan',
   password: 'skam123',
   database: 'HackTues'
 })
 
-connection.connect(function(err) {
-  if (err)throw err;
-  console.log("Connected!");
-  
-  var sql = "INSERT INTO users (ID, EmailAddress, PasswordSalt,PasswordHash) VALUES ('','','','')";
-  connection.query(sql, function (err, result) {
-    if (err)throw err ;
-    console.log("1 record inserted");
-    connection.on('error', function(err) {
-      console.log("[mysql error]",err);
-    });
+/*app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/register.html'));
   });
+app.post('/login.html',function(req,res){
+
+    var email=req.body.email;
+    var password=req.body.password;
+    res.sendFile('/login.html',{root:__dirname});
+    var i =Number(14);  
+    connection.connect(function(err) {
+    if (err) throw err;
+    var sql = "INSERT INTO users (ID,EmailAddress, PasswordSalt,PasswordHash) VALUES ('"+i+"', '"+email+"','"+password+"','')";
+    connection.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("1 record inserted");
+       res.end();
+       i++;
+    });
+    });
+  })*/
+  app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/register.html'));
+  
+  connection.query('select * from users where EmailAddress= "'+req.body.email+'" ',function(err,row){
+    if(err){
+      connnection.query('insert into users(ID,EmailAddress, PasswordSalt,PasswordHash) VALUES("","' + password + '","' + email + '","")',
+      function(err,rows,fields){   
+        console.log(rows);
+        res.send("inserted");
+    });
+  }
+  else{
+    res.send("user already registered");
+    return;
+  }
+  });
+
+  connection.end();
 });
