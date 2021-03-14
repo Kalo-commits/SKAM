14052d1a38163f6dda61c00d01f2f58a4a173300
AHumanBarcode <73434365+AHumanBarcode@users.noreply.github.com>
2021-03-13 21:28:56 -0800

    Added handler for servo request and servo motion

--- NodeJS/HackTues.js
+++ NodeJS/HackTues.js
@@ -15,6 +15,11 @@ app.get('/signup.html', (req, res) => {
     res.sendFile('./signup.html', { root: __dirname });
 });
 
+app.get('/servo.txt', function(req,res){
+   	 res.send('The request has been prossessed and the watering system has been opened for 2 seconds.');
+	dt.Turn();
+});
+
 app.get('/login.html', (req, res) => {
     res.sendFile('./login.html', { root: __dirname });
 });
@@ -79,4 +84,3 @@ app.post('/login.html',function(req,res){
     });
     });
   });
-  dt.Code();
